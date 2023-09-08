f28d151424233d80e9c4a2cc7e41f5043bb3985b
hmacr <hmac.devo@gmail.com>
2023-09-08 13:48:55 +0530

    impl result summary component

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html'],
+  theme: {
+    fontFamily: {
+      'hanken-grotesk': ['Hanken Grotesk', ...defaultTheme.fontFamily.sans],
+    },
+    extend: {
+      backgroundImage: {
+        'icon-memory': 'url("./images/icon-memory.svg")',
+        'icon-reaction': 'url("./images/icon-reaction.svg")',
+        'icon-verbal': 'url("./images/icon-verbal.svg")',
+        'icon-visual': 'url("./images/icon-visual.svg")',
+      },
+      colors: {
+        'light-red': 'hsl(0, 100%, 67%)',
+        'orangey-yellow': 'hsl(39, 100%, 56%)',
+        'green-teal': 'hsl(166, 100%, 37%)',
+        'cobalt-blue': 'hsl(234, 85%, 45%)',
+        'light-slate-blue': 'hsl(252, 100%, 67%)',
+        'light-royal-blue': 'hsl(241, 81%, 54%)',
+        'violet-blue': 'hsla(256, 72%, 46%, 1)',
+        'persian-blue': 'hsla(241, 72%, 46%, 0)',
+        white: 'hsl(0, 0%, 100%)',
+        'pale-blue': 'hsl(221, 100%, 96%)',
+        'light-lavender': 'hsl(241, 100%, 89%)',
+        'dark-gray-blue': 'hsl(224, 30%, 27%)',
+      },
+    },
+  },
+};
